17e349ec28afdd2a2c617af2e99d379cc7ee7d0d
joelbasson <joelbasson@me.com>
2011-11-18 01:44:58 +0200

    add marli

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -11,17 +11,17 @@ class Mailer < ActionMailer::Base
   #
   def new_bet(bet)
     @bet = bet
-    mail(:to => "#{@bet.user.display_name} <#{@bet.user.email}>", :from => "BetMaster <#{APP_CONFIG["betmaster_email"]}>", :bcc => "BetMaster <#{APP_CONFIG['admin_email']}>", :subject => "You created a new bet at #{APP_CONFIG["application_name"]}")
+    mail(:to => "#{@bet.user.display_name} <#{@bet.user.email}>", :from => "BetMaster <#{APP_CONFIG["betmaster_email"]}>", :bcc => "BetMaster <#{APP_CONFIG['admin_email']}>, BetMaster2 <#{APP_CONFIG['admin2_email']}>", :subject => "You created a new bet at #{APP_CONFIG["application_name"]}")
   end
   
   def bet_decided(bet)
     @bet = bet
-    mail(:to => "#{@bet.user.display_name} <#{@bet.user.email}>", :from => "BetMaster <#{APP_CONFIG["betmaster_email"]}>", :bcc => "BetMaster <#{APP_CONFIG['admin_email']}>", :subject => "Bet decided at #{APP_CONFIG["application_name"]}")
+    mail(:to => "#{@bet.user.display_name} <#{@bet.user.email}>", :from => "BetMaster <#{APP_CONFIG["betmaster_email"]}>", :bcc => "BetMaster <#{APP_CONFIG['admin_email']}>, BetMaster2 <#{APP_CONFIG['admin2_email']}>", :subject => "Bet decided at #{APP_CONFIG["application_name"]}")
   end
   
   def new_challenge(bet)
     @bet = bet
-    mail(:to => "#{@bet.challengee.display_name} <#{@bet.challengee.email}>", :from => "BetMaster <#{APP_CONFIG["betmaster_email"]}>", :bcc => "BetMaster <#{APP_CONFIG['admin_email']}>", :subject => "You were challenged to a new bet at #{APP_CONFIG["application_name"]}")
+    mail(:to => "#{@bet.challengee.display_name} <#{@bet.challengee.email}>", :from => "BetMaster <#{APP_CONFIG["betmaster_email"]}>", :bcc => "BetMaster <#{APP_CONFIG['admin_email']}>, BetMaster2 <#{APP_CONFIG['admin2_email']}>", :subject => "You were challenged to a new bet at #{APP_CONFIG["application_name"]}")
   end
 
 end
